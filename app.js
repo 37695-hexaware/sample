db8349369c5d70566a4c7a2fc797fd3e3d94eaa1
37695-hexaware <37695.hexaware@gmail.com>
2017-06-16 16:57:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+const REST_PORT = (process.env.PORT || 3000); 
+ 
+
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 })
