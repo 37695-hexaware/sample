f12c04022c74c3908606eb3edcd7fec13b9cd319
37695-hexaware <37695.hexaware@gmail.com>
2017-07-21 15:57:06 +0530

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,69 +1,62 @@
 
+/*
+ HTTP Cloud Function.
+
+ @param {Object} req Cloud Function request context.
+ @param {Object} res Cloud Function response context.
+*/
+/*
+exports.helloHttp = function helloHttp (req, res) {
+  response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+
+
+res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+  res.send(JSON.stringify({ "speech": response, "displayText": response 
+  //"speech" is the spoken version of the response, "displayText" is the visual version
+  }));
+};
+
+-----
+*/
+
 'use strict';
 var apiai = require('apiai');
- 
-var appai = apiai("67d9605566d14a29ba1197e2e44dc2a5");
- 
+
+var appai = apiai("baf498c6d40a4a70b54ef80debdcbc33");
+
 var express=require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var portC = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
- 
+
 var options = {
     sessionId: '567yh8'
 };
- 
- 
- 
- 
-function simpleResponse (req, res) {
-  console.log('inside simpleResponse');
-  const appapi = new ApiAiApp({request: req, response: res});
-  const WELCOME_INTENT = 'input.welcome';
-  const LOOKUP_INTENT = 'input.lookup_citidata';
-  const actionMap = new Map();
-  actionMap.set(WELCOME_INTENT, welcomeIntent);
-  actionMap.set(LOOKUP_INTENT, lookupdataIntent);
-  appapi.handleRequest(actionMap);
- 
- 
-};
- 
- 
- 
-app.post('/',function(req,res){
+
+
+
+
+   app.post('/',function(req,res){
       console.log('REQUESTTTTT:::'+JSON.stringify(req.body));
-      if(req.body.result.parameters.company_name)
-      // || (req.body.result.parameters.date_time) || (req.body.result.parameters.financial_balance_sheet_data)
-        {
-        var resagent="Sure, will check the details  from " + req.body.result.parameters.company_name+ " to "+
-      req.body.result.parameters.date_time+
-        "   for "+ req.body.result.parameters.financial_balance_sheet_data +"  .....";
- 
- 
-       processJSON(clientName, financialYear, metricsKey);
- 
-      }
-      else{
-      var resagent="Alright, will check for  " + req.body.result.parameters.company_name+
-            "   for"+req.body.result.parameters.date_time +"  ...";
-     }
+      if(req.body.result.parameters.location1){
+        var resagent="Sure, will check the details  from " + req.body.result.parameters.location+ " to "+
+        req.body.result.parameters.location1+
+        "   for "+ req.body.result.parameters.date +"  .....";
+      }else{
+   var resagent="Alright, will check for tkts to " + req.body.result.parameters.location+
+   "   for"+ req.body.result.parameters.date +"   How would you like to travel?by flight,train or bus.....";
+}
       return res.json({
         speech:resagent,
         displayText: resagent,
         source:'hello ticket book'
       });
- 
+
 });
- 
- 
- 
- 
- 
- 
- 
 app.listen(portC, function(){
     console.log('AGENT is running my app on  PORT: ' + portC);
 });
+
+
